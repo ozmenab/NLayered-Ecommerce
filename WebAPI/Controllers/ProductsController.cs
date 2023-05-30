13978f65ba19d4c7f569d0bb4865b69f1ed21375
pc <ozmenab@gmail.com>
2023-05-30 12:03:29 +0300

    Add WebAPI Layer

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> GetAll()
+        {
+            return _productService.GetAll();
+        }
+        [HttpPost]
+        public Product Add(Product product)
+        {
+            ValidationTool.Validate(new ProductValidator(), product);
+            return _productService.Add(product);
+        }
+    }
+}
